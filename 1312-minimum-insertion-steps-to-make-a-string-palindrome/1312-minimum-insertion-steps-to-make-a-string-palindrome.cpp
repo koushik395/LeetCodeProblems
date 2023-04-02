9de1d6e8a2c7fac3f6e412d52a020993c4fc315d
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-04-02 13:58:21 +0530

    Time: 122 ms (9.87%), Space: 28.5 MB (24.86%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int lcs(string text1, string text2,int m,int n)
+    {
+        vector<vector<int>> dp(m+1,vector<int>(n+1));
+        for(int i=0;i<m+1;i++)
+        {
+            for(int j=0;j<n+1;j++)
+            {
+                if(i==0 || j==0)
+                    dp[i][j] =0;
+                else if(text1[i-1] == text2[j-1])
+                    dp[i][j] = 1+ dp[i-1][j-1];
+                else
+                {
+                    int a = dp[i-1][j];
+                    int b = dp[i][j-1];
+                    dp[i][j] = max(a,b);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+    int minInsertions(string s) {
+        int m = s.length();
+        string s2 = s;
+        reverse(s2.begin(),s2.end());
+        return s.length() - lcs(s,s2,m,m);
+    }
+};
