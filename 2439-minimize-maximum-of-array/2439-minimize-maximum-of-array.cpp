4fce823a85ab47d8461f0f6c1148f90b1c3e2be8
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-04-05 23:12:40 +0530

    Time: 136 ms (79.77%), Space: 71.4 MB (61.54%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -1,29 +1,11 @@
 class Solution {
 public:
-    bool solve(int max, vector<int> &a) {
-        long ex = 0;
-        for (int i : a) {
-            if (i > max) {
-                long c = (i - max);
-                if (c > ex) return false;
-                ex -= c;
-            } else {
-                ex += (max - i);
-            }
-        }
-        return true;
-    }
     int minimizeArrayValue(vector<int>& nums) {
-        int s = 0, e = (int) 1e9, ans = 0;
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
-            if (solve(mid, nums)) {
-                e = mid - 1;
-                ans = mid;
-            } else {
-                s = mid + 1;
-            }
+        long sum = 0, res = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            sum += nums[i];
+            res = max(res, (sum + i) / (i + 1));
         }
-        return ans;
+        return res;
     }
 };
