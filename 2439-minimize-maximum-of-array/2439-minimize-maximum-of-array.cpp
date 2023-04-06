c5e5d9f503a84853ac415a6dce7e858ae6046810
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-04-06 19:28:24 +0530

    Time: 131 ms (85.84%), Space: 71.4 MB (62.06%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -4,7 +4,7 @@ public:
         long sum = 0, res = 0;
         for (int i = 0; i < nums.size(); ++i) {
             sum += nums[i];
-            res = max(res, (sum + i) / (i + 1));
+            res = max(res, (long)ceil((double)sum/(i+1)));
         }
         return res;
     }
