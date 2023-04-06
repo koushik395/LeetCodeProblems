99689ea95a59078715aea36103958d6e72c6a001
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-04-06 18:48:03 +0530

    Time: 3 ms (41.73%), Space: 5.9 MB (75.49%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int kthGrammar(int N, int K) {
+        // visualise in form of binary tree
         if (N == 1) return 0;
-        if (K % 2 == 0) return (kthGrammar(N - 1, K / 2) == 0) ? 1 : 0;
-        else return (kthGrammar(N - 1, (K + 1) / 2) == 0) ? 0 : 1;
+        if (K % 2 == 0) return (kthGrammar(N - 1, K / 2) == 0) ? 1 : 0;//right child
+        else return (kthGrammar(N - 1, (K + 1) / 2) == 0) ? 0 : 1;//left child
     }
 };
