18c8c1ed8e6b599d9656a6eb6e61d6fbb765aefb
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-04-06 19:31:58 +0530

    Time: 144 ms (65.26%), Space: 71.4 MB (28.67%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int minimizeArrayValue(vector<int>& nums) {
-        long sum = 0, res = 0;
+        long long sum = 0, res = 0;
         for (int i = 0; i < nums.size(); ++i) {
             sum += nums[i];
-            res = max(res, (long)ceil((double)sum/(i+1)));
+            res = max(res, (long long)ceil((double)sum/(i+1)));
         }
         return res;
     }
