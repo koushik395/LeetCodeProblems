38ea899fc931593466d1441b77adfa1f8e1e8b4f
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-04-08 00:16:16 +0530

    Time: 4 ms (14.02%), Space: 5.8 MB (95.85%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int comp = 1;
+        while (comp < num) 
+            comp = (comp << 1) | 1;
+        return num ^ comp;
+    }
+};
