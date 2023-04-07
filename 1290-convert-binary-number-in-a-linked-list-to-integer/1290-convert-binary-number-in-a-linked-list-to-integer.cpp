618e2a8367f5ea15944a02fa81a12a51a8ca93c3
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-04-07 22:40:36 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (25.30%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -10,26 +10,7 @@
  */
 class Solution {
 public:
-    int ans = 0;
-    int i =-1;
-    // void reverse(ListNode* &head,ListNode* curr,ListNode* prev)
-    // {
-    //     if(curr==NULL)
-    //     {
-    //         head = prev;
-    //         return;
-    //     }
-    //     ListNode* temp = curr->next;
-    //     reverse(head,temp,curr);
-    //     curr->next = prev;
-    //     i++;
-    //     ans = ans + curr->val * pow(2,i);
-    // }
     int getDecimalValue(ListNode* head) {
-        // ListNode* curr=head;
-        // ListNode* prev = NULL;
-        // reverse(head,curr,prev);
-        // return ans;
         unsigned int ans = 0;
         while(head!=NULL)
         {
