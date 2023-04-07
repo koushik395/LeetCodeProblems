4a05bf60a3e0637d21789585919a715bd53d46df
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-04-07 20:26:41 +0530

    Time: 52 ms (22.23%), Space: 45 MB (15.09%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void generate(vector<int>& nums, int n, int target, vector<int>& subset) {
-        if (target == 0) {
-            ans.push_back(subset);
+    void generate(vector<int>& nums,int n,int target,vector<int> subsets)
+    {
+        if(target == 0)
+        {
+            ans.push_back(subsets);
             return;
         }
-        if (n == 0 || target < 0) {
+        if(n == 0)
             return;
+        
+        if(nums[n-1] > target)
+            generate(nums,n-1,target,subsets);
+        else {
+            subsets.push_back(nums[n-1]);
+            generate(nums,n,(target-nums[n-1]),subsets);
+            subsets.pop_back();
+            generate(nums,n-1,target,subsets);
         }
-        // Include the current element and recurse with the same index
-        subset.push_back(nums[n-1]);
-        generate(nums, n, target-nums[n-1], subset);
-        subset.pop_back();
-        // Exclude the current element and recurse with the previous index
-        generate(nums, n-1, target, subset);
     }
-
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int> subsets;
         int n = candidates.size();
-        generate(candidates, n, target, subsets);
+        generate(candidates,n,target,subsets);
         return ans;
     }
 
