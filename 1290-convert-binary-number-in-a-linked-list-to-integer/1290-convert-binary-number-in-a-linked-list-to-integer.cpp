2a4d030dfad447810236455b1f23756f524f9847
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-04-07 22:36:20 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (70.38%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -12,23 +12,31 @@ class Solution {
 public:
     int ans = 0;
     int i =-1;
-    void reverse(ListNode* &head,ListNode* curr,ListNode* prev)
-    {
-        if(curr==NULL)
-        {
-            head = prev;
-            return;
-        }
-        ListNode* temp = curr->next;
-        reverse(head,temp,curr);
-        curr->next = prev;
-        i++;
-        ans = ans + curr->val * pow(2,i);
-    }
+    // void reverse(ListNode* &head,ListNode* curr,ListNode* prev)
+    // {
+    //     if(curr==NULL)
+    //     {
+    //         head = prev;
+    //         return;
+    //     }
+    //     ListNode* temp = curr->next;
+    //     reverse(head,temp,curr);
+    //     curr->next = prev;
+    //     i++;
+    //     ans = ans + curr->val * pow(2,i);
+    // }
     int getDecimalValue(ListNode* head) {
-        ListNode* curr=head;
-        ListNode* prev = NULL;
-        reverse(head,curr,prev);
+        // ListNode* curr=head;
+        // ListNode* prev = NULL;
+        // reverse(head,curr,prev);
+        // return ans;
+        unsigned int ans = 0;
+        while(head!=NULL)
+        {
+            ans <<= 1;
+            ans = (ans | (head->val));
+            head = head->next;
+        }
         return ans;
     }
 };
