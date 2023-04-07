fc0707eb23a7135bb795c977407846a50fc19806
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-04-07 12:25:43 +0530

    Time: 64 ms (96.79%), Space: 30.3 MB (48.11%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        int ans=0;
+        for(int i = 0;i< rows;i++)
+        {
+            for(int j = 0;j < cols;j++)
+            {
+                if(grid[i][j] == 1)
+                    if(i==0 || i == rows-1 || j==0 || j == cols-1)
+                        FloodFill(grid,i,j,rows,cols);
+            }
+        }
+        for(auto &row:grid)
+            for(auto &col:row)
+                if(col == 1)
+                    ans++;
+        return ans;
+        
+    }
+    void FloodFill(vector<vector<int>>& grid,int i,int j,int rows,int cols)
+    {   
+        if(i>=0 && i<rows && j>=0 && j<cols && grid[i][j]==1)
+        {
+            grid[i][j] = 0;
+            FloodFill(grid,i-1,j,rows,cols);
+            FloodFill(grid,i+1,j,rows,cols);
+            FloodFill(grid,i,j+1,rows,cols);
+            FloodFill(grid,i,j-1,rows,cols);   
+        }
+    }
+    
+};
