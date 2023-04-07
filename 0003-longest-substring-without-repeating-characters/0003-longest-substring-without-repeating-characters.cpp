c953644c06b458a3569c169620975c3616cc68c2
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-04-07 19:45:17 +0530

    Time: 31 ms (36.96%), Space: 10.8 MB (27.21%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,22 +3,18 @@ public:
     int lengthOfLongestSubstring(string s) {
         if(!s.size())
             return 0;
-        bitset<50001> flag;
-        bitset<50001> str(1);
-        int i = 0,j = 0,ans = 1;
-         while((i <=j) && (j < s.size())){
-            int val = s[j];
-            int x = (flag & (str<<val)).count();
-            if(x) {
-                int dup = s[i];
-                flag.reset(dup);
+        unordered_set<char> str;
+        int i = 0,j=0,ans = 1;
+        while((i <= j) && (j<s.size()))
+        {
+            if(str.find(s[j])!= str.end())
+            {
+                str.erase(s[i]);
                 i++;
             }else{
-                flag = flag | (str<<val);
-                int size = flag.count();
+                str.insert(s[j]);
+                int size = str.size();
                 ans = max(ans,size);
-                str.reset();
-                str.set(0);
                 j++;
             }
         }
