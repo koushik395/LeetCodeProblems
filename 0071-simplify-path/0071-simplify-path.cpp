1769b751da4d40bf3b4b5b32ac2142851caab3a0
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-04-12 22:09:18 +0530

    Time: 4 ms (78.99%), Space: 9.5 MB (66.72%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -9,7 +9,7 @@ public:
             if (tmp == ".." and !stk.empty()) stk.pop_back();
             else if (tmp != "..") stk.push_back(tmp);
         }
-        for(auto str : stk) res += "/"+str;
+        for(auto &str : stk) res += "/"+str;
         return res.empty() ? "/" : res;
         
     }
