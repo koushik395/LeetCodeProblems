c2e993c62867f8b32a596e36460a97e91d9e54ce
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-04-18 20:12:33 +0530

    Time: 2 ms (50.88%), Space: 6.6 MB (12.30%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -4,7 +4,7 @@ public:
         int i=0,j=0;
         string res = "";
         while(i<word1.length() && j<word2.length())
-            res = res + word1[i++] + word2[j++];
+            res += string() + word1[i++] + word2[j++];
         return res + word1.substr(i) + word2.substr(j);
     }
 };
