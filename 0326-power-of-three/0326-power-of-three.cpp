745474b924ebd19a5e08dede2f29a55d88d9fc93
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-04-18 22:04:54 +0530

    Time: 36 ms (18.79%), Space: 5.9 MB (61.46%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        /*For any M <= N that is a power of 3, M divides N.
-          For any M <= N that is not a power 3, M does not divide N*/
+        /*For any N <= M that is a power of 3, M divides N.
+          For any N <= M that is not a power 3, M does not divide N
+          M = 3^20(as it is the highest num in int range).*/
+        
         return (n>0 && 3486784401%n==0);
     }
 };
