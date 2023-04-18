46eed81b812015214411705a0d1554e20e9ef413
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-04-18 22:02:50 +0530

    Time: 32 ms (30.07%), Space: 6 MB (61.46%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
+        /*For any M <= N that is a power of 3, M divides N.
+          For any M <= N that is not a power 3, M does not divide N*/
         return (n>0 && 3486784401%n==0);
     }
 };
