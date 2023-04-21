3b3bcb82a7e4f4ccbd07cb960d1ff89596e58b08
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-04-21 18:33:35 +0530

    Time: 185 ms (70.68%), Space: 92.7 MB (73.69%) - LeetHub

---
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int longestCycle(vector<int>& edges) {
+        int v = edges.size();
+        vector<int> indegree(v, 0);
+        vector<bool> visited(v,false);
+        int ans = -1;
+
+        for (int i = 0; i < v; i++) {
+            if(edges[i]!=-1)
+                indegree[edges[i]]++;
+        }
+
+        queue<int> q;
+        for(int i=0;i<v;i++)
+        {
+            if(indegree[i]==0)
+                q.push(i);
+        }
+
+        while(!q.empty())
+        {
+            int front = q.front();
+            visited[front] = true;
+            q.pop();
+            int child = edges[front];
+            if(child!=-1)
+            {
+                indegree[child]--;
+                if(indegree[child]==0) q.push(child);    
+            }  
+        }
+        
+        for(int i=0;i<v;i++)
+        {
+           if(!visited[i])
+           {
+               int neighbour = edges[i];
+               int count = 1;
+               visited[i] = true;
+               while(neighbour!=i)
+               {
+                   visited[neighbour] = true;
+                   count++;
+                   neighbour = edges[neighbour];
+               }
+               ans = max(ans,count);
+           }
+        }
+        return ans;
+    }
+};
