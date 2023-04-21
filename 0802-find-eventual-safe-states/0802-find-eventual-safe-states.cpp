853dea7c29d1a3b6b4d8b95989036f3184c6ac9c
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-04-21 17:14:02 +0530

    Time: 207 ms (45.77%), Space: 61.7 MB (18.16%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,38 +1,15 @@
 class Solution {
 public:
-    void isCyclic(vector<vector<int>>& graph, int node, vector<bool>& visited, unordered_map<int, vector<int>>& parent) {
-    visited[node] = true;
-
-    for (int i = 0; i < graph[node].size(); i++) {
-        int neighbor = graph[node][i];
-        if (!visited[neighbor]) {
-            parent[neighbor].push_back(node);
-            isCyclic(graph, neighbor, visited, parent);
-        } else {
-            parent[neighbor].push_back(node); 
-        }
-    }
-}
-
-
 vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
     int v = graph.size();
     vector<int> outdegree(v, 0);
+    vector<vector<int>> parent(v);
     vector<int> ans;
 
     for (int i = 0; i < v; i++) {
         for (int neighbour : graph[i]) {
             outdegree[i]++;
-        }
-    }
-
-    vector<bool> visited(v, false);
-    unordered_map<int, vector<int>> parent;
-
-    for (int i = 0; i < v; i++) {
-        if (!visited[i]) {
-            parent[i].push_back(-1);
-            isCyclic(graph, i, visited, parent);
+            parent[neighbour].push_back(i);
         }
     }
 
