7560537c39de67982f84c5acc54e93294cb3a9be
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-04-22 22:32:42 +0530

    Time: 171 ms (94.14%), Space: 60.6 MB (93.04%) - LeetHub

---
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void dfs(vector<int> adj[], vector<bool>& hasApple, int start, vector<bool>& visited, int& time)
+{
+    visited[start] = true;
+    for (auto& neighbour : adj[start])
+    {
+        if (!visited[neighbour])
+        {
+            dfs(adj, hasApple, neighbour, visited, time);
+            if (hasApple[neighbour])
+            {
+                time += 2; // Increment time by 2 for each edge traversal
+                hasApple[start] = true; // Mark the current vertex as having an apple
+            }
+        }
+    }
+}
+
+    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple)
+    {
+        vector<int> adj[n];
+        vector<bool> visited(n, false);
+        int time = 0;
+        for (auto& v : edges)
+        {
+            adj[v[0]].push_back(v[1]);
+            adj[v[1]].push_back(v[0]);
+        }
+        dfs(adj, hasApple, 0, visited, time);
+        return time;
+    }
+
+};
