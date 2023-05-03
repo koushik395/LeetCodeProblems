f910fe7c7a8551855efcf37e346d3dac3db5a2b7
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-05-03 20:44:15 +0530

    Time: 9 ms (22.97%), Space: 13.8 MB (52.49%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -10,27 +10,37 @@
  * };
  */
 class Solution {
+    int n,inStart,inEnd;
+    unordered_map<int,int> NodeToIndex;
 public:
-    TreeNode* insertIntoTree(TreeNode* &root,int data)
+    void findPos(vector<int> &inorder,unordered_map<int,int> &NodeToIndex)
     {
-        if(root == NULL)
+        for(int i =0;i<inorder.size();i++)
         {
-            TreeNode* node = new TreeNode(data);
-            return node;
+            NodeToIndex[inorder[i]] = i;
         }
-
-        if(data < root->val)
-            root->left = insertIntoTree(root->left,data);
-
-        if(data > root->val)
-            root->right = insertIntoTree(root->right,data);
-
+    }
+    TreeNode* solve(vector<int>& preorder, vector<int>& inorder,int &index,int inStart,int inEnd)
+    {
+        if(index >= n || inStart > inEnd)
+            return NULL;
+        
+        int element = preorder[index++];
+        TreeNode* root = new TreeNode(element);
+        int position = NodeToIndex[element];
+        
+        root->left = solve(preorder,inorder,index,inStart,position-1);
+        root->right = solve(preorder,inorder,index,position+1,inEnd);
+        
         return root;
     }
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        TreeNode* root = NULL;
-        for(auto& num:preorder)
-            root = insertIntoTree(root,num);
-        return root;
+        vector<int> inorder = preorder;
+        sort(inorder.begin(),inorder.end());
+        int preorderIndex = 0;
+        n = preorder.size();
+        findPos(inorder,NodeToIndex);
+        TreeNode* ans = solve(preorder,inorder,preorderIndex,0,n-1);
+        return ans;
     }
 };
