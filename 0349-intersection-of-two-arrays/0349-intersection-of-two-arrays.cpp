952373538d36d6795f1cd7df1137e95493e78076
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-05-11 21:12:34 +0530

    Time: 5 ms (71.17%), Space: 10.1 MB (71.80%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        
+        set<int> result;
+        int i = 0,j=0;
+        int n = nums1.size(),m = nums2.size();
+        while(i < n && j < m)
+        {
+            if(nums1[i] < nums2[j]) i++;
+            else if(nums2[j] < nums1[i]) j++;
+            else
+            {
+                result.insert(nums1[i]);
+                i++,j++;
+            }
+        }
+        vector<int> ans(result.begin(),result.end());
+        return ans;
+    }
+};
