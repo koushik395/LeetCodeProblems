8ef4584702699bae2fbd4d17e1641ff75e33b4ba
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-05-22 17:15:27 +0530

    Time: 407 ms (13.71%), Space: 117.1 MB (50.67%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,82 +1,25 @@
 class MedianFinder {
 public:
-    double median;
-    vector<int> nums;
-    priority_queue<double> maxheap;
-    priority_queue<double, vector<double>,greater<double>> minheap;
-    int i;
-    
-    int signum(int a,int b)
-    {
-        if(a==b) return 0;
-        return a > b? 1: -1;
-    }
-    
-    void callMedian(int &element,priority_queue<double> &maxheap,priority_queue<double, vector<double>,greater<double>> &minheap,double &median)
-    {
-        switch(signum(maxheap.size(),minheap.size()))
-        {
-            case 0: if(element > median){
-                        minheap.push(element);
-                        median = minheap.top();
-                    }
-                    else{
-                        maxheap.push(element);
-                        median = maxheap.top();
-                    }
-                    break;
-            case 1:
-                    if(element > median){
-                        minheap.push(element);
-                        median = (maxheap.top()+minheap.top())/2;
-                    }
-                    else{
-                        minheap.push(maxheap.top());
-                        maxheap.pop();
-                        maxheap.push(element);
-                        median = (maxheap.top()+minheap.top())/2;
-                    }
-                    break;
-            case -1:
-                    if(element > median){
-                        maxheap.push(minheap.top());
-                        minheap.pop();
-                        minheap.push(element);
-                        median = (maxheap.top()+minheap.top())/2;
-                    }
-                    else{
-                        maxheap.push(element);
-                        median = (maxheap.top()+minheap.top())/2;
-                    }
-                    break;
-         }
-    }
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
     MedianFinder() {
-        median = 0;
-        i = -1;
+
     }
     
     void addNum(int num) {
-        i++;
-        nums.push_back(num);
+        maxHeap.push(num);
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+        if (minHeap.size() > maxHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
     }
     
     double findMedian() {
-        if(!maxheap.size() && !minheap.size())
-        {
-            for(auto& num:nums)
-            {
-                callMedian(num,maxheap,minheap,median);
-            }
-        }
-        else
-        {
-            for(int j = i;j < nums.size();j++)
-            {
-                callMedian(nums[j],maxheap,minheap,median);   
-            }   
-        }
-        return median;
+        if (maxHeap.size() > minHeap.size()) return maxHeap.top();
+        return (maxHeap.top() + minHeap.top()) / 2.0;
     }
 };
 
