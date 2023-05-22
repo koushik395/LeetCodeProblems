6b4b0078fb51c6e6073405aafd925540754d8e14
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-05-22 15:48:01 +0530

    Added solution - LeetHub

--- Merge k Sorted Arrays - GFG/merge-k-sorted-arrays.cpp
+++ Merge k Sorted Arrays - GFG/merge-k-sorted-arrays.cpp
@@ -14,22 +14,55 @@ for (int i=0; i < size; i++)
 // } Driver Code Ends
 //User function Template for C++
 
+class node{
+    public:
+        int data;
+        int i;
+        int j;
+    node(int data,int i ,int j)
+    {
+        this->data = data;
+        this->i = i;
+        this->j = j;
+    }
+};
+
+class compare{
+    public:
+        bool operator()(node* a,node* b)
+        {
+            return a->data > b ->data;   
+        }
+};
 
 class Solution
 {
     public:
     //Function to merge k sorted arrays.
-    vector<int> mergeKArrays(vector<vector<int>> arr, int K)
+    vector<int> mergeKArrays(vector<vector<int>> arr, int k)
     {
-        vector<int> ans;
-        for(auto& row:arr)
+        priority_queue<node*,vector<node*>,compare> minHeap;
+        for(int i = 0;i<k;i++)
         {
-            for(auto&num : row)
-            {
-                ans.push_back(num);
+            node* temp = new node(arr[i][0],i,0);
+            minHeap.push(temp);
+        }
+        
+        vector<int> ans;
+        while(minHeap.size() > 0)
+        {
+            node* tmp = minHeap.top();
+            ans.push_back(tmp->data);
+            minHeap.pop();
+            
+            int i = tmp -> i;
+            int j = tmp ->j;
+            
+            if(j+1 < arr[i].size()){
+                node* next = new node(arr[i][j+1],i,j+1);
+                minHeap.push(next);
             }
         }
-        sort(ans.begin(),ans.end());
         return ans;
     }
 };
