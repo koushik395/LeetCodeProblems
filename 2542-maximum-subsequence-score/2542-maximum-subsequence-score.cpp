b46946084a17449097da1871aea2d4fd57426824
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-05-24 19:13:39 +0530

    Time: 934 ms (5.17%), Space: 87.5 MB (83.00%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
@@ -7,7 +7,7 @@ public:
         for(int i = 0;i < nums1.size();i++)
                 newarr[i] = make_pair(nums2[i],nums1[i]);
         
-        sort(newarr.begin(),newarr.end(),greater<pair<int,int>>());
+        sort(newarr.rbegin(),newarr.rend());
         long long sum = 0,ans = 0;
         
         for(auto& [mul,score]: newarr)
