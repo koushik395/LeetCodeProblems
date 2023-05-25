3387a6e4ef8c19d440354fe245fcc1e8bdd5a79b
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-05-25 22:46:10 +0530

    Time: 1032 ms (48.12%), Space: 592.4 MB (34.48%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,81 @@
+class TrieNode{
+public:
+	char data;
+	TrieNode* children[26];
+	bool isTerminal;
+
+	TrieNode(char ch)
+	{
+		data = ch;
+		for(int i = 0;i<26;i++)
+		{
+			children[i] = NULL;
+		}
+		isTerminal = false;
+	}
+
+};
+
+class WordDictionary {
+public:
+    TrieNode* root;
+    WordDictionary() {
+        root = new TrieNode('\0');
+    }
+    
+    void insertUtil(TrieNode* root,string word)
+	{
+		if(word.length() == 0) {
+			root->isTerminal = true;
+			return;
+		}
+		int index = word[0] - 'a';
+
+		TrieNode* child;
+		if(root->children[index]!=NULL)
+		{
+			//child is present
+			child = root->children[index];
+		}
+		else
+		{
+			//child absent
+			child = new TrieNode(word[0]);
+			root->children[index] = child;
+		}
+
+		insertUtil(child,word.substr(1));
+	}
+    
+    void addWord(string word) {
+        insertUtil(root,word);
+    }
+    
+    bool searchUtil(TrieNode* root, string word) {
+        for (int i = 0; i < word.length() && root; i++) {
+            if (word[i] != '.') {
+                int index = word[i] - 'a';
+                root = root->children[index];
+            } else {
+                for (int j = 0; j < 26; j++) {
+                    if (searchUtil(root->children[j], word.substr(i + 1))) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+        return root && root->isTerminal;
+    }
+    
+    bool search(string word) {
+        return searchUtil(root,word);
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
