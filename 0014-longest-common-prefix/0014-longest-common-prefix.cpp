ca108e366c9a6f6bfd92987f8a08e8551e7c0829
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-05-25 21:58:36 +0530

    Time: 27 ms (5.18%), Space: 16.1 MB (6.48%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,17 +1,96 @@
+class TrieNode{
+public:
+	char data;
+	TrieNode* children[26];
+	bool isTerminal;
+    int childcount;
+	TrieNode(char ch)
+	{
+		data = ch;
+		for(int i = 0;i<26;i++)
+		{
+			children[i] = NULL;
+		}
+        childcount = 0;
+		isTerminal = false;
+	}
+
+};
+
+class Trie {
+
+public:
+    TrieNode* root;
+    /** Initialize your data structure here. */
+    Trie() {
+        root = new TrieNode('\0');
+    }
+
+    void insertUtil(TrieNode* root,string word)
+	{
+		if(word.length() == 0) {
+			root->isTerminal = true;
+			return;
+		}
+		//word will be in caps
+		int index = word[0] - 'a';
+
+		TrieNode* child;
+		if(root->children[index]!=NULL)
+		{
+			//child is present
+			child = root->children[index];
+		}
+		else
+		{
+			//child absent
+			child = new TrieNode(word[0]);
+            root->childcount++;
+			root->children[index] = child;
+		}
+
+		insertUtil(child,word.substr(1));
+	}
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        insertUtil(root,word);
+    }
+    
+    void LCP(string str,string &ans)
+    {
+        for(int i = 0;i < str.length();i++)
+        {
+            char ch = str[i];
+            
+            if(root->childcount == 1) 
+            {
+                ans.push_back(ch);
+                int index = ch - 'a';
+                root = root->children[index];
+            }
+            else
+                break;
+            
+            if(root->isTerminal) break;
+        }
+    }
+};
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        sort(strs.begin(),strs.end());
-        int c = 0;
-        string first = strs[0];
-        string last = strs[strs.size()-1];
+        Trie* t = new Trie();
         
-        while(c < first.length())
+        for(auto& str:strs)
         {
-            if(first[c] == last[c]) c++;
-            else break;
+            if(str == "") return "";
+            t->insert(str);
         }
         
-        return c == 0? "":first.substr(0,c);
+        string first = strs[0];
+        string ans = "";
+        
+        t->LCP(first,ans);
+        return ans;
     }
 };
