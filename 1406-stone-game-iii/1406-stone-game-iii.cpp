c37b0ea95b8f4cb4ebc2c9d3a8b1c56a4c1f3288
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-05-27 20:03:09 +0530

    Time: 311 ms (29.38%), Space: 126.4 MB (97.87%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     string stoneGameIII(vector<int>& stoneValue) {
         int n = stoneValue.size();
-        vector<int> dp(n+1,0);
+        vector<int> dp(3,0);
         
         for(int i = n-1; i>= 0;i--)
         {
-            int takeone = stoneValue[i] - dp[i+1];
+            int takeone = stoneValue[i] - dp[(i+1)%3];
         
             int taketwo = INT_MIN;
-            if(i+1 < n) taketwo = stoneValue[i] + stoneValue[i+1] - dp[i+2];
+            if(i+1 < n) taketwo = stoneValue[i] + stoneValue[i+1] - dp[(i+2)%3];
 
             int takethree = INT_MIN;
-            if(i+2 < n) takethree = stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - dp[i+3];
+            if(i+2 < n) takethree = stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - dp[(i+3)%3];
 
-            dp[i] = max({takeone,taketwo,takethree});    
+            dp[i%3] = max({takeone,taketwo,takethree});    
         }
         
         int value = dp[0];
