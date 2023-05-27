3abe7d44b02efd4fea960e82d4203d28d8544f22
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-05-27 19:56:56 +0530

    Time: 302 ms (33.41%), Space: 132.1 MB (81.04%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    int helper(vector<int> &dp,vector<int> &stoneValue,int i)
-    {
-        int n = stoneValue.size();
-        if(i >= n) return 0;
-        
-        if(dp[i]!=INT_MAX) return dp[i];
-        
-        int takeone = stoneValue[i] - helper(dp,stoneValue,i+1);
-        
-        int taketwo = INT_MIN;
-        if(i+1 < n) taketwo = stoneValue[i] + stoneValue[i+1] - helper(dp,stoneValue,i+2);
-        
-        int takethree = INT_MIN;
-        if(i+2 < n) takethree = stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - helper(dp,stoneValue,i+3);
-        
-        return dp[i] = max({takeone,taketwo,takethree});
-    }
     string stoneGameIII(vector<int>& stoneValue) {
-        vector<int> dp(stoneValue.size(),INT_MAX);
-        int value = helper(dp,stoneValue,0);
+        int n = stoneValue.size();
+        vector<int> dp(n+1,0);
+        
+        for(int i = n-1; i>= 0;i--)
+        {
+            int takeone = stoneValue[i] - dp[i+1];
+        
+            int taketwo = INT_MIN;
+            if(i+1 < n) taketwo = stoneValue[i] + stoneValue[i+1] - dp[i+2];
+
+            int takethree = INT_MIN;
+            if(i+2 < n) takethree = stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - dp[i+3];
+
+            dp[i] = max({takeone,taketwo,takethree});    
+        }
+        
+        int value = dp[0];
         if(value > 0)
             return "Alice";
         return value < 0 ? "Bob":"Tie";
