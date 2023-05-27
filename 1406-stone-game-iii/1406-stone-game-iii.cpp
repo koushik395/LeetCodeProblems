4c741eaed02103392454a11a23f5e0e35d9fd494
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-05-27 19:46:19 +0530

    Time: 330 ms (23.93%), Space: 148 MB (15.88%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int helper(vector<int> &dp,vector<int> &stoneValue,int i)
+    {
+        int n = stoneValue.size();
+        if(i >= n) return 0;
+        
+        if(dp[i]!=0) return dp[i];
+        
+        int takeone = stoneValue[i] - helper(dp,stoneValue,i+1);
+        
+        int taketwo = INT_MIN;
+        if(i+1 < n) taketwo = stoneValue[i] + stoneValue[i+1] - helper(dp,stoneValue,i+2);
+        
+        int takethree = INT_MIN;
+        if(i+2 < n) takethree = stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - helper(dp,stoneValue,i+3);
+        
+        return dp[i] = max({takeone,taketwo,takethree});
+    }
+    string stoneGameIII(vector<int>& stoneValue) {
+        vector<int> dp(stoneValue.size(),0);
+        int value = helper(dp,stoneValue,0);
+        if(value > 0)
+            return "Alice";
+        return value < 0 ? "Bob":"Tie";
+    }
+};
