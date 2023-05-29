0f8d449d8605c10c64a5054a27a58b9e8c81a141
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-05-29 21:32:42 +0530

    Time: 353 ms (94.00%), Space: 8 MB (80.25%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,46 +1,45 @@
 class Solution {
 public:
-    bool isPerimeter(int i,int j,int rows,int cols)
-    {
-        return (i>=0 && i < rows && j >=0 && j < cols);
+    bool isPerimeter(int i, int j, int rows, int cols) {
+        return (i >= 0 && i < rows && j >= 0 && j < cols);
     }
 
-    bool isPossible(vector<vector<char>>& grid,int i,int j,int rows,int cols,string word)
-    {
-        if(word.length() == 0) return true;
-
-        if(!isPerimeter(i,j,rows,cols)) return false;
-        
-        bool left = 0,right = 0,up = 0,down = 0;
-
-        if(grid[i][j] == word[0])
-        {
-            char temp = word[0]; 
-            grid[i][j] = ' ';
-            word = word.substr(1);
-            left = isPossible(grid,i,j-1,rows,cols,word);
-            right = isPossible(grid,i,j+1,rows,cols,word);
-            up = isPossible(grid,i-1,j,rows,cols,word);
-            down = isPossible(grid,i+1,j,rows,cols,word);
-            grid[i][j] = temp;
+    bool isPossible(vector<vector<char>>& grid, int i, int j, int rows, int cols, const string& word, int index) {
+        if (index == word.length()) {
+            return true;
         }
-        return left || right || up || down;
+
+        if (!isPerimeter(i, j, rows, cols) || grid[i][j] != word[index]) {
+            return false;
+        }
+
+        char temp = grid[i][j];
+        grid[i][j] = ' ';
+
+        bool found = isPossible(grid, i, j - 1, rows, cols, word, index + 1) ||
+                     isPossible(grid, i, j + 1, rows, cols, word, index + 1) ||
+                     isPossible(grid, i - 1, j, rows, cols, word, index + 1) ||
+                     isPossible(grid, i + 1, j, rows, cols, word, index + 1);
+
+        grid[i][j] = temp;
+        return found;
     }
-    bool exist(vector<vector<char>>& board, string word) {
+
+    bool exist(vector<vector<char>>& board, const string& word) {
         int rows = board.size();
         int cols = board[0].size();
 
-        for(int i = 0;i< rows;i++)
-        {
-            for(int j = 0;j < cols;j++)
-            {
-                if(board[i][j]==word[0])
-                {
-                    if(isPossible(board,i,j,rows,cols,word))
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (board[i][j] == word[0]) {
+                    if (isPossible(board, i, j, rows, cols, word, 0)) {
                         return true;
+                    }
                 }
             }
         }
+
         return false;
     }
+
 };
