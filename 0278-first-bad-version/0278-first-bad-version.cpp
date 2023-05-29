21fc6fa8d47684f3a61e10545c78029401a71fd1
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-05-29 20:34:27 +0530

    Time: 0 ms (100.00%), Space: 6 MB (8.51%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -4,10 +4,19 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        if(isBadVersion(n))
+        // if(isBadVersion(n))
+        // {
+        //     return firstBadVersion(--n);
+        // }
+        // return n+1;
+        
+        int low = 0,high = n,mid;
+        while(low < high)
         {
-            return firstBadVersion(--n);
+            mid = low + (high - low)/2;
+            if(isBadVersion(mid)) high = mid;
+            else low = mid + 1;
         }
-        return n+1;
+        return low;
     }
 };
