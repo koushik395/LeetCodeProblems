357790720566e6078b4fad0857ed218de1309d26
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-06-03 20:38:41 +0530

    Time: 0 ms (100.00%), Space: 7 MB (86.54%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.cpp
+++ 0980-unique-paths-iii/0980-unique-paths-iii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int dfs(vector<vector<int>>& g, int i, int j, int s, int empcells) {
           if (i < 0 || j < 0 || i >= g.size() || j >= g[0].size() || g[i][j] == -1) return 0;
-          if (g[i][j] == 2) return s == empcells-1 ? 1 : 0;
+          if (g[i][j] == 2) return s == empcells-1 ? 1 : 0; //if steps equals emptycells path++
           g[i][j] = -1;
           int paths = dfs(g, i + 1, j, s + 1, empcells) + dfs(g, i - 1, j, s + 1, empcells) +
             dfs(g, i, j + 1, s + 1, empcells) + dfs(g, i, j - 1, s + 1, empcells);
@@ -14,7 +14,7 @@ public:
       for (auto i = 0; i < g.size(); ++i)
         for (auto j = 0; j < g[0].size(); ++j) {
           if (g[i][j] == 1) x = i, y = j;
-          if (g[i][j] != -1) ++empcells;
+          if (g[i][j] != -1) ++empcells; //count emptycells
         }
       return dfs(g, x, y, 0, empcells);
     }
