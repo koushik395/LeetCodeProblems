54b4be818c8f1b7de0e15719407d5f42b4b70424
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-06-05 13:09:46 +0530

    Time: 7 ms (90.62%), Space: 9.9 MB (91.90%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -3,7 +3,7 @@ public:
     pair<int,int> distance(vector<int>& first,vector<int>& second)
     {
         int x1 = first[0],y1 = first[1],x2 = second[0],y2 = second[1];
-        return {(y2-y1),(x2-x1),};
+        return {(y2-y1),(x2-x1)};
     }
     bool checkStraightLine(vector<vector<int>>& nums) {
         pair<int,int> firstDist = distance(nums[0],nums[1]);
