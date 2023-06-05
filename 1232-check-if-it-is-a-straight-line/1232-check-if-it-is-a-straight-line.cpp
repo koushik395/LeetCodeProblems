13cbafd82f76f39fbe6033254bc01c7680643c20
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-06-05 13:08:50 +0530

    Time: 12 ms (39.13%), Space: 10 MB (91.90%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
+    pair<int,int> distance(vector<int>& first,vector<int>& second)
+    {
+        int x1 = first[0],y1 = first[1],x2 = second[0],y2 = second[1];
+        return {(y2-y1),(x2-x1),};
+    }
     bool checkStraightLine(vector<vector<int>>& nums) {
-        int dY = nums[1][1] - nums[0][1];
-        int dX = nums[1][0] - nums[0][0];     
-        for( int i=2; i < nums.size(); i++ ) {
-            auto p = nums[i];
-            if( dX*(p[1] - nums[0][1]) != dY*(p[0] - nums[0][0]) )
-                return false;
+        pair<int,int> firstDist = distance(nums[0],nums[1]);
+        int dy = firstDist.first;
+        int dx = firstDist.second;
+        
+        for(int i=2;i < nums.size();i++)
+        {
+            pair<int,int> nextDist =  distance(nums[0],nums[i]);
+            if(dx*(nextDist.first) != dy*(nextDist.second)) return false;
         }
         return true;
     }
