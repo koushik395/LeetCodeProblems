9a12aab569eb2fbf8b217cf1e1721d9a36086ac6
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-06-12 16:30:08 +0530

    Time: 135 ms (16.67%), Space: 91.8 MB (100.00%) - LeetHub

--- 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.cpp
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.cpp
@@ -1,7 +1,20 @@
 class Solution {
 public:
     int findNonMinOrMax(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        return nums.size() > 2 ? nums[1]:-1;
+        if(nums.size() < 3)
+        {
+            return -1;
+        }
+        int maxi = max({nums[0],nums[1],nums[2]});
+        int mini = min({nums[0],nums[1],nums[2]});
+        
+        for(int i = 0; i< 3;i++)
+        {
+            if(mini < nums[i] && nums[i] < maxi)
+            {
+                return nums[i];
+            }
+        }
+        return -1;
     }
 };
