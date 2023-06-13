4417decd91ff6ab2de374439df1e18730b2c9656
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-06-13 23:10:55 +0530

    Time: 3 ms (52.74%), Space: 6 MB (50.00%) - LeetHub

--- 0754-reach-a-number/0754-reach-a-number.cpp
+++ 0754-reach-a-number/0754-reach-a-number.cpp
@@ -1,19 +1,7 @@
 class Solution {
 public:
-    bool isPossible(int start, int mid, int target) {
-        for (int i = 1; i <= mid + 1; i++) {
-            if (abs(start) == target)
-                return true;
-            if (start + abs(i) <= target)
-                start = start + abs(i);
-            else
-                start = start - abs(i);
-        }
-        return false;
-    
-    }
     int reachNumber(int target) {
-        target = abs(target); // Make target positive
+        target = abs(target);
 
         int sum = 0;
         int steps = 0;
