4c49e801c14d3d519d7674299b2c02430b2527bc
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-06-20 00:03:37 +0530

    Time: 44 ms (88.78%), Space: 18.2 MB (51.34%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,70 @@
+class DSU {
+public: 
+    vector<int> parent, size; 
+    DSU(int n) {
+        parent.resize(n+1);
+        size.resize(n+1); 
+        for(int i = 0;i<=n;i++) {
+            parent[i] = i; 
+            size[i] = 1; 
+        }
+    }
+
+    int findParent(int node) {
+        if(node == parent[node])
+            return node; 
+        return parent[node] = findParent(parent[node]); 
+    }
+    
+    void unionBySize(int u, int v) {
+        int ulp_u = findParent(u); 
+        int ulp_v = findParent(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+            size[ulp_v] += size[ulp_u]; 
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v]; 
+        }
+    }
+};
+
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int maxrow = 0;
+        int maxcol = 0;
+        int n = stones.size();
+        
+        for(auto& it: stones)
+        {
+            maxrow = max(maxrow,it[0]);
+            maxcol = max(maxcol,it[1]);
+        }
+        
+        DSU ds(maxrow+maxcol+1);
+        
+        unordered_map<int,int> stoneNodes;
+        for(auto& it:stones)
+        {
+            int nodeRow = it[0];
+            int nodeCol = it[1] + maxrow + 1;
+            ds.unionBySize(nodeRow,nodeCol);
+            stoneNodes[nodeRow] = 1;
+            stoneNodes[nodeCol] = 1;
+        }
+        
+        int cnt = 0;
+        for(auto& it: stoneNodes)
+        {
+            if(ds.findParent(it.first) == it.first)
+            {
+                cnt++;
+            }
+        }
+        return n - cnt; // number of stones - number of connected components
+        // for each component of size n we can remove n-1 stones
+    }
+};
