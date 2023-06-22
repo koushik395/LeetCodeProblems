163d418d3d3b0b3daf06a871f188880ff9186a01
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-06-22 22:35:44 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (77.38%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -5,7 +5,7 @@ public:
         int prev1 = nums[0],prev2 = 0;
         for(int i = 1;i < n;i++)
         {
-            int take = nums[i] + ((i > 1)?prev2:0);
+            int take = nums[i] + prev2;
             int nottake = prev1;
             int curr = max(take,nottake);
             prev2 = prev1, prev1 = curr;
