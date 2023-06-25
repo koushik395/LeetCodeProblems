47de30bf50fb35a8b7af48a3d2abd8f4593a3cc7
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-06-25 22:45:12 +0530

    Time: 10 ms (24.55%), Space: 8.9 MB (48.99%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -23,7 +23,7 @@ public:
         
         for(int i = n-2;i >= 0;i--)
         {
-            for(int j = i;j >= 0;j--)
+            for(int j = 0;j <= i;j++)
             {
                 int d = triangle[i][j] + dp[i+1][j];
                 int dia = triangle[i][j] + dp[i+1][j+1];
