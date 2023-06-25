c182f4b8a7af686efacf0800e04b689ff65a68b7
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-06-25 23:36:34 +0530

    Time: 25 ms (9.64%), Space: 10.7 MB (22.86%) - LeetHub

---
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+//     int find(int i,int j,vector<vector<int>>& matrix,int n,int m,vector<vector<int>> &dp)
+//     {
+//         if(j < 0 || j >= m) return 1e9;
+//         if(i == 0) return matrix[0][j];
+        
+//         if(dp[i][j]!=-1) return dp[i][j];
+        
+//         int straight = matrix[i][j] + find(i-1,j,matrix,n,m,dp);
+//         int leftdiag = matrix[i][j] + find(i-1,j-1,matrix,n,m,dp);
+//         int rightdia = matrix[i][j] + find(i-1,j+1,matrix,n,m,dp);
+        
+//         return dp[i][j] = min({straight,leftdiag,rightdia});
+//     }
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size(),m = matrix[0].size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        
+        for(int j = 0;j < m;j++)
+        {
+            dp[0][j] = matrix[0][j];
+        }
+        
+        for(int i = 1; i < n;i++)
+        {
+            for(int j = 0; j< m;j++)
+            {
+                int straight = matrix[i][j] + dp[i-1][j];
+                int leftdiag = matrix[i][j];
+                if(j-1 >= 0) leftdiag += dp[i-1][j-1];
+                else leftdiag += 1e9;
+                
+                int rightdia = matrix[i][j];
+                if(j+1 < m) rightdia += dp[i-1][j+1];
+                else rightdia += 1e9;
+                
+                dp[i][j]  = min({straight,leftdiag,rightdia});          
+            }
+        }
+        int mini = dp[n-1][0];
+        for(int j = 1; j < m;j++)
+        {
+            mini = min(mini,dp[n-1][j]);
+        }
+        
+        return mini;
+    }
+};
