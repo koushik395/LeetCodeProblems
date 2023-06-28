365115a4c4195a0d3e85137b84fced22af99155a
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-06-28 23:37:45 +0530

    Added solution - LeetHub

--- Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
+++ Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
@@ -4,43 +4,39 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-  const int M=1e9 +7;
   public:
-    int isSubsetSum(vector<int>arr, int sum){
-    int n = arr.size();
-    vector<vector<int>> dp(n+1,vector<int>(sum+1));
-    for(int i=0;i<n+1;i++)
-    {
-        for(int j=0;j<sum+1;j++)
-        {
-            if(i==0 && j==0)
-                dp[i][j] = 1;
-            else if(i==0)
-                dp[i][j] = 0;
-            // else if(j==0)
-            //     dp[i][j] = 1;
-            else
-            {
-                if(arr[i-1] > j)
-                    dp[i][j] = dp[i-1][j];
-                else
-                {
-                    int take = dp[i-1][j-arr[i-1]];
-                    int nottake = dp[i-1][j];
-                    dp[i][j] = (take+nottake)%M;   
-                }
-            }
+      int M = 1e9+7;
+      int findWays(vector<int> &num, int k){
+        int n = num.size();
+    
+        vector<vector<int>> dp(n,vector<int>(k+1,0));
+        
+        if(num[0] == 0) dp[0][0]  = 2;
+        else dp[0][0] = 1;
+    
+        if(num[0]!=0 && num[0]<=k)
+            dp[0][num[0]] = 1;
+    
+        for(int ind = 1; ind<n; ind++){
+            for(int target= 0; target<=k; target++){
+                
+                int notTaken = dp[ind-1][target];
+        
+                int taken = 0;
+                    if(num[ind]<=target)
+                        taken = dp[ind-1][target-num[ind]];
             
+                dp[ind][target]= (notTaken + taken)%M;
+            }
         }
+        
+        return dp[n-1][k];
     }
-    return dp[n][sum];
-}
     int countPartitions(int n, int d, vector<int>& arr) {
-        int sum = accumulate(arr.begin(),arr.end(),0);
-    	if((sum+d) & 1)
-    		return 0;
-    	int reqsum = (sum+d)>>1;
-        return isSubsetSum(arr,reqsum);
+        int tot = accumulate(arr.begin(),arr.end(),0);
+        if(tot- d < 0 || (tot-d)%2) return 0;
+    
+        return findWays(arr, (tot-d)/2);
     }
 };
 
