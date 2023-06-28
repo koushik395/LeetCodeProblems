0e9bb58194cb2f10d790af336c9ae0c720c2f831
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-06-28 23:01:58 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.cpp
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -5,29 +5,30 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
 	public:
-	const int  M = 1e9+7; 
-	int perfectSum(int arr[], int n, int sum)
+	const int M = 1e9+7;
+	int findWaysUtil(int ind, int target, int arr[], vector<vector<int>> &dp){
+        if(ind == 0)
+        {
+            if(target == 0 && arr[0] == 0) return 2;
+            if(target == 0 || target == arr[0]) return 1; 
+            return 0;
+        }
+        
+        if(dp[ind][target]!=-1)
+            return dp[ind][target];
+            
+        int notTaken = findWaysUtil(ind-1,target,arr,dp);
+        
+        int taken = 0;
+        if(arr[ind]<=target)
+            taken = findWaysUtil(ind-1,target-arr[ind],arr,dp);
+            
+        return dp[ind][target]= (notTaken + taken)%M;
+    }
+	int perfectSum(int num[], int n, int k)
 	{
-	    vector<vector<int>> dp(n+1,vector<int>(sum+1));
-	    for(int i=0;i< n+1;i++)
-	    {
-	        for(int j=0;j<sum+1;j++)
-	        {
-	            if(i==0 && j==0)
-	                dp[i][j] = 1;
-	            else if(i==0)
-	                dp[i][j] = 0;
-	            else if(arr[i-1]>j)
-	                dp[i][j] = dp[i-1][j];
-	            else
-	            {
-	                int take = dp[i-1][j-arr[i-1]];
-	                int nottake = dp[i-1][j];
-	                dp[i][j] = (take + nottake)%M;
-	            }
-	        }
-	    }
-        return dp[n][sum];
+        vector<vector<int>> dp(n,vector<int>(k+1,-1));
+        return findWaysUtil(n-1,k,num,dp);
 	}
 	  
 };
