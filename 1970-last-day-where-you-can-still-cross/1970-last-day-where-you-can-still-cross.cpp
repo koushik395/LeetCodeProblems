c089d172523e5db9d00ea05554595df82ebca88d
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-06-30 21:44:34 +0530

    Time: 413 ms (72.52%), Space: 107.8 MB (73.42%) - LeetHub

--- 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
@@ -1,28 +1,35 @@
 class DSU {
-private:
-    vector<int> p;
-
-public:
-    DSU(int N) {
-        p.resize(N);
-        for (int i = 0; i < N; i++) {
-            p[i] = i;
+    vector<int> parent, size; 
+public: 
+    DSU(int n) {
+        parent.resize(n+1);
+        size.resize(n+1); 
+        for(int i = 0;i<=n;i++) {
+            parent[i] = i; 
+            size[i] = 1; 
         }
     }
 
-    int find(int x) {
-        if (p[x] != x) {
-            p[x] = find(p[x]);
+    int findParent(int node) {
+        if(node == parent[node])
+            return node; 
+        return parent[node] = findParent(parent[node]); 
+    }
+    
+    void unionBySize(int u, int v) {
+        int ulp_u = findParent(u); 
+        int ulp_v = findParent(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+            size[ulp_v] += size[ulp_u]; 
         }
-        return p[x];
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v]; 
+        }
     }
-
-    void unionSet(int x, int y) {
-        int xr = find(x);
-        int yr = find(y);
-        p[xr] = yr;
-    }
-};
+}; 
 
 class Solution {
 public:
@@ -54,17 +61,17 @@ public:
                 int newY = y + dy;
                 int ind = index(newX, newY);
                 if (newX >= 0 && newX < n && newY >= 0 && newY < m && grid[newX][newY] == 0) {
-                    dsu.unionSet(ind, index(x, y));
+                    dsu.unionBySize(ind, index(x, y));
                 }
             }
             if (x == 0) {
-                dsu.unionSet(0, index(x, y));
+                dsu.unionBySize(0, index(x, y));
             }
             if (x == n - 1) {
-                dsu.unionSet(m * n + 1, index(x, y));
+                dsu.unionBySize(m * n + 1, index(x, y));
             }
 
-            if (dsu.find(0) == dsu.find(m * n + 1)) {
+            if (dsu.findParent(0) == dsu.findParent(m * n + 1)) {
                 return i;
             }
         }
