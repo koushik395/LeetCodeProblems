e941867967ec070730cbe083381cfe887a903309
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-07-03 13:20:38 +0530

    Time: 531 ms (9.16%), Space: 7.4 MB (92.00%) - LeetHub

---
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> direc = {{-1,0},{1,0},{0,-1},{0,1}};
+    int dfs(int i,int j,vector<vector<int>> &grid,int n,int m)
+    {
+        if(i < 0 || j < 0 || i == n || j == m || grid[i][j] == 0) return 0;
+        
+        int total = 0;
+        total = grid[i][j];
+        int val = grid[i][j];
+        grid[i][j] = 0;
+        
+        int maxi = -1;
+        for(auto& it: direc)
+        {
+            int newx = i + it[0];
+            int newy = j + it[1];
+            maxi = max(maxi,total + dfs(newx,newy,grid,n,m));
+        }
+        
+        grid[i][j] = val;
+        return maxi;
+    }
+    int getMaximumGold(vector<vector<int>>& grid) {
+        int n  = grid.size();
+        int m  = grid[0].size();
+        
+        int maxi = 0;
+        for(int i = 0;i < n;i++)
+        {
+            for(int j = 0;j < m;j++)
+            {
+                if(grid[i][j] > 0)
+                {
+                    maxi = max(maxi,dfs(i,j,grid,n,m));   
+                }
+            }
+        }
+        return maxi;
+    }
+};
