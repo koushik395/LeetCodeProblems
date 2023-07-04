846dac6de57138db182ce51cca9ac9361cfdee42
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-07-04 12:29:27 +0530

    Time: 8 ms (67.31%), Space: 9.4 MB (72.62%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -6,13 +6,13 @@ public:
         {
             for(int i=0;i<32;i++)
             {
-                bits[i] += ((num >> i) & 1);
+                bits[i] += ((num >> i) & 1); //count the number of 1 bits for every number at the corresponding position.
             }
         }
         
         for(int i=0;i<32;i++)
         {
-            ans |= ((bits[i]%3) << i);
+            ans |= ((bits[i]%3) << i); // do %3 because for the elements that appear three times the result at that bit will be 0. The duplicate element appears only once and hence doing %3 gives the corresponding bit value at that position.
         }
         return ans;
     }
