b21e90930958c5443cb1af81912eb9959699340e
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-07-05 23:13:08 +0530

    Time: 43 ms (45.12%), Space: 7.3 MB (85.39%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,28 +1,48 @@
 class Solution {
 public:
-    int minimumElementsUtil(vector<int>& arr, int ind, int T, vector<vector<int>>& dp){
+//     int minimumElementsUtil(vector<int>& arr, int ind, int T, vector<vector<int>>& dp){
 
-        if(ind == 0){
-            if(T%arr[0] == 0) return 1;
-            else return 0;
+//         if(ind == 0){
+//             if(T%arr[0] == 0) return 1;
+//             else return 0;
+//         }
+
+//         if(dp[ind][T]!=-1)
+//             return dp[ind][T];
+
+//         int notTaken = minimumElementsUtil(arr,ind-1,T,dp);
+
+//         int taken = 0;
+//         if(arr[ind] <= T)
+//             taken = minimumElementsUtil(arr,ind,T-arr[ind],dp);
+
+//         return dp[ind][T] = notTaken + taken;
+// }
+    int change(int T, vector<int>& arr) {
+        int n= arr.size();
+    
+        vector<int> prev(T+1,0), cur(T+1,0);
+
+        for(int i=0; i<=T; i++){
+            if(i%arr[0] == 0)  
+                prev[i] = 1;
+            else prev[i] = 0;
         }
 
-        if(dp[ind][T]!=-1)
-            return dp[ind][T];
+        for(int ind = 1; ind<n; ind++){
+            for(int target = 0; target<=T; target++){
 
-        int notTaken = minimumElementsUtil(arr,ind-1,T,dp);
+                int notTake = 0 + prev[target];
+                int take = 0;
+                if(arr[ind]<=target)
+                    take = cur[target - arr[ind]];
 
-        int taken = 0;
-        if(arr[ind] <= T)
-            taken = minimumElementsUtil(arr,ind,T-arr[ind],dp);
+                 cur[target] = take + notTake;
+            }
+            prev = cur;
+        }
 
-        return dp[ind][T] = notTaken + taken;
-}
-    int change(int amount, vector<int>& coins) {
-        int n = coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        int ans= minimumElementsUtil(coins,n-1,amount,dp);
-        
+        int ans = prev[T];
         return ans;
     }
 };
