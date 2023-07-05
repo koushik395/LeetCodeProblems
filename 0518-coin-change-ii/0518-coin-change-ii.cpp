6cca9b32c779363d083f9454927bac8d6aa4ff98
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-07-05 23:10:38 +0530

    Time: 11 ms (87.53%), Space: 18.5 MB (36.37%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
+    int minimumElementsUtil(vector<int>& arr, int ind, int T, vector<vector<int>>& dp){
+
+        if(ind == 0){
+            if(T%arr[0] == 0) return 1;
+            else return 0;
+        }
+
+        if(dp[ind][T]!=-1)
+            return dp[ind][T];
+
+        int notTaken = minimumElementsUtil(arr,ind-1,T,dp);
+
+        int taken = 0;
+        if(arr[ind] <= T)
+            taken = minimumElementsUtil(arr,ind,T-arr[ind],dp);
+
+        return dp[ind][T] = notTaken + taken;
+}
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n+1,vector<int>(amount+1));
-        for(int i = 0;i<n+1;i++)
-        {
-            for(int j=0;j<amount+1;j++)
-            {
-                if(i == 0)
-                    dp[i][j] = 0;
-                else if(j == 0)
-                    dp[i][j] = 1;
-                else{
-                    if(coins[i-1]>j)
-                        dp[i][j] = dp[i-1][j];
-                    else
-                    {
-                        int take = dp[i][j-coins[i-1]];
-                        int nottake = dp[i-1][j];
-                        dp[i][j] = take+nottake;
-                    }
-                }
-            }
-        }
-        return dp[n][amount];
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        int ans= minimumElementsUtil(coins,n-1,amount,dp);
+        
+        return ans;
     }
 };
