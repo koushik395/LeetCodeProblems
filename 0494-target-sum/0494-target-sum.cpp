8b0ef3d8a6fd72335b33ac3b12ba2e9e6e924ab2
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-07-05 23:20:09 +0530

    Time: 15 ms (74.60%), Space: 11.9 MB (56.43%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,40 +1,35 @@
 class Solution {
 public:
-    int isSubsetSum(vector<int>arr, int sum){
-    int n = arr.size();
-    vector<vector<int>> dp(n+1,vector<int>(sum+1));
-    for(int i=0;i<n+1;i++)
-    {
-        for(int j=0;j<sum+1;j++)
-        {
-            if(i==0 && j==0)
-                dp[i][j] = 1;
-            else if(i==0)
-                dp[i][j] = 0;
-            else
-            {
-                if(arr[i-1] > j)
-                    dp[i][j] = dp[i-1][j];
-                else
-                {
-                    int take = dp[i-1][j-arr[i-1]];
-                    int nottake = dp[i-1][j];
-                    dp[i][j] = (take+nottake);   
-                }
-            }
-            
-        }
-    }
-    return dp[n][sum];
-}
+    int findWays(vector<int> &num, int k){
+        int n = num.size();
 
+        vector<vector<int>> dp(n,vector<int>(k+1,0));
+
+        if(num[0] == 0) dp[0][0]  = 2;
+        else dp[0][0] = 1;
+
+        if(num[0]!=0 && num[0]<=k)
+            dp[0][num[0]] = 1;
+
+        for(int ind = 1; ind<n; ind++){
+            for(int target= 0; target<=k; target++){
+
+                int notTaken = dp[ind-1][target];
+
+                int taken = 0;
+                    if(num[ind]<=target)
+                        taken = dp[ind-1][target-num[ind]];
+
+                dp[ind][target]= (notTaken + taken);
+            }
+        }
+
+        return dp[n-1][k];
+    }
     int findTargetSumWays(vector<int>& arr, int d) {
-        if(d<0)
-            d*=-1;
-        int sum = accumulate(arr.begin(),arr.end(),0);
-    	if((sum+d) & 1)
-    		return 0;
-    	int reqsum = (sum+d)>>1;
-        return isSubsetSum(arr,reqsum);
+        int tot = accumulate(arr.begin(),arr.end(),0);
+        if(tot- d < 0 || (tot-d)%2) return 0;
+
+        return findWays(arr, (tot-d)/2);
     }
 };
