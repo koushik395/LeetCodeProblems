8c315d2065b4c79666a2375c78446374f5c9a744
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-07-06 21:56:44 +0530

    Added solution - LeetHub

---
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int f(int ind,int w,int profit[],int weight[],vector<vector<int>> &dp)
+    {
+        if(ind == 0)
+        {
+            return (w/weight[0]) * profit[0];
+        }
+    
+        if(dp[ind][w]!=-1) return dp[ind][w];
+    
+        int nottake = 0 + f(ind-1,w,profit,weight,dp);
+        int take = INT_MIN;
+        if(weight[ind] <= w)
+        {
+            take = profit[ind] + f(ind,w-weight[ind],profit,weight,dp);
+        }
+    
+        return dp[ind][w] = max(take,nottake);
+    }
+    int knapSack(int n, int w, int profit[], int weight[])
+    {
+        vector<vector<int>> dp(n,vector<int>(w+1,-1));
+        return f(n-1,w,profit,weight,dp);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N, W;
+        cin>>N>>W;
+        int val[N], wt[N];
+        for(int i = 0;i < N;i++)
+            cin>>val[i];
+        for(int i = 0;i < N;i++)
+            cin>>wt[i];
+        
+        Solution ob;
+        cout<<ob.knapSack(N, W, val, wt)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
