03890036c8b1287db79af94e8fc14061112c6ce4
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-07-11 12:31:26 +0530

    Time: 12 ms (18.55%), Space: 13.3 MB (28.63%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,83 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> ans;
+    void parent(TreeNode* root,unordered_map<TreeNode*,TreeNode*> &parentNode)     {
+        if(root == NULL)
+            return;
+        if(root->left)
+        {
+            parentNode[root->left] = root;
+            parent(root->left,parentNode);
+        }
+        if(root->right)
+        {
+            parentNode[root->right] = root;
+            parent(root->right,parentNode);
+        }
+    }
+    
+    void burnTree(TreeNode* root,unordered_map<TreeNode*,TreeNode*> &parentNode,int k){
+        unordered_map<TreeNode*,bool> visited;
+        queue<TreeNode*> q;
+        
+        q.push(root);
+        visited[root] = true;
+        
+        int dist = 0;
+        
+        TreeNode* front;
+        while(!q.empty())
+        {
+            int size = q.size();
+            bool flag = false;
+            for(int i=0;i<size;i++)
+            {
+                front = q.front();
+                q.pop();
+                
+                if(dist == k) ans.push_back(front->val);
+                
+                if(front->left && !visited[front->left])
+                {
+                    flag = true;
+                    q.push(front->left);
+                    visited[front->left] = true;
+                }
+                if(front->right && !visited[front->right])
+                {
+                    flag = true;
+                    q.push(front->right);
+                    visited[front->right] = true;
+                }
+                if(parentNode[front] && !visited[parentNode[front]])
+                {
+                    flag = true;
+                    q.push(parentNode[front]);
+                    visited[parentNode[front]] = true;
+                }
+            }
+            if(flag == true)
+            {
+                dist++;
+            }
+        }
+    }
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        unordered_map<TreeNode*,TreeNode*> parentNode;
+        parentNode[root] = NULL;
+        parent(root,parentNode);
+        
+        burnTree(target,parentNode,k);
+        return ans;
+        
+    }
+};
