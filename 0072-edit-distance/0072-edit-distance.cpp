9fcb1fbf799388e2d3f5f31f7b1e6d3bd857dfba
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-07-12 23:25:43 +0530

    Time: 17 ms (52.71%), Space: 6.4 MB (93.14%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
-    int f(int i,int j,string &s1,string &s2,vector<vector<int>> &dp)
-    {
-        if(i == 0) return j; //insertion
-        if(j == 0) return i; //deletion
+//     int f(int i,int j,string &s1,string &s2,vector<vector<int>> &dp)
+//     {
+//         if(i == 0) return j; //insertion
+//         if(j == 0) return i; //deletion
         
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s1[i-1] == s2[j-1]) return dp[i][j] = f(i-1,j-1,s1,s2,dp);
+//         if(dp[i][j] != -1) return dp[i][j];
+//         if(s1[i-1] == s2[j-1]) return dp[i][j] = f(i-1,j-1,s1,s2,dp);
         
-        return dp[i][j] = 1 + min({f(i-1,j,s1,s2,dp),f(i,j-1,s1,s2,dp),f(i-1,j-1,s1,s2,dp)});
-                                  //deletion            insertion           replace
-    }
+//         return dp[i][j] = 1 + min({f(i-1,j,s1,s2,dp),f(i,j-1,s1,s2,dp),f(i-1,j-1,s1,s2,dp)});
+//                                   //deletion            insertion           replace
+//     }
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        for(int j = 0;j <= m;j++) dp[0][j] = j;
-        for(int i = 1;i <= n;i++) dp[i][0] = i;
+        vector<int> prev(m+1,0),curr(m+1,0);
+        for(int j = 0;j <= m;j++) prev[j] = j;
         
         for(int i = 1;i <= n;i++)
         {
+            curr[0] = i;
             for(int j = 1;j <= m;j++)
             {
                  if(word1[i-1] == word2[j-1]) {
-                     dp[i][j] = dp[i-1][j-1];
+                     curr[j] = prev[j-1];
                  }
                 else
                 {
-                    dp[i][j] = 1 + min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
+                    curr[j] = 1 + min({prev[j],curr[j-1],prev[j-1]});
                 }
             }
+            prev = curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
