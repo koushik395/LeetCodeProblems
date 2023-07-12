5a1f747dc7b3e6d836d56af366cccac42043d822
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-07-12 23:16:07 +0530

    Time: 27 ms (28.52%), Space: 9 MB (61.19%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -16,7 +16,7 @@ public:
         int m = word2.size();
         vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
         for(int j = 0;j <= m;j++) dp[0][j] = j;
-        for(int i = 0;i <= n;i++) dp[i][0] = i;
+        for(int i = 1;i <= n;i++) dp[i][0] = i;
         
         for(int i = 1;i <= n;i++)
         {
