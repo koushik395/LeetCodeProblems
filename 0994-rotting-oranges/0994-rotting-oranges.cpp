c98b0cdc62644f5b8c9a14890b93c758d8bf5bda
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-07-13 22:06:07 +0530

    Time: 12 ms (28.10%), Space: 13.3 MB (36.80%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -28,12 +28,12 @@ public:
         int tm = 0;
 
         int cnt = 0; //to track no. of fresh oranges visited
-
+        int t = -1;
         while (!q.empty()) {
           int r = q.front().first.first;
           int c = q.front().first.second;
-          int t = q.front().second;
-          tm = max(tm, t);
+          t = q.front().second;
+          // tm = max(tm, t);
           q.pop();
 
           for(auto& dir: directions) {
@@ -54,6 +54,6 @@ public:
         // if all oranges are not rotten
         if (cnt != cntFresh) return -1;
 
-        return tm;
+        return t == -1?0:t;
     }
 };
