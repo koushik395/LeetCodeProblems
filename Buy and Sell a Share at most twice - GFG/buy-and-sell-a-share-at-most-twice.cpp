cf7cf0d8e160080a2057a7e0165abbb8ef1cd230
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-07-23 12:38:31 +0530

    Added solution - LeetHub

---
+++ Buy and Sell a Share at most twice - GFG/buy-and-sell-a-share-at-most-twice.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+int maxProfit(vector<int>&prices){
+    int n = prices.size();
+    vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+    for(int ind = n-1;ind >= 0;ind--)
+    {
+        for(int buy = 0;buy <= 1;buy++)
+        {
+            for(int cap = 1;cap <= 2;cap++)
+            {
+                if(buy)
+                {
+                    dp[ind][buy][cap] = max(-prices[ind] + dp[ind+1][0][cap],0 + dp[ind+1][1][cap]);
+                }
+                else
+                {
+                    dp[ind][buy][cap] = max(prices[ind] + dp[ind+1][1][cap-1],0 + dp[ind+1][0][cap]);
+                }
+            }
+        }
+    }
+    
+    return dp[0][1][2];
+}
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        
+        int n;
+        cin>>n;
+        vector<int> price(n);
+        for(int i=0;i<n;i++) cin>>price[i];
+        cout<<maxProfit(price)<<endl;
+    }
+
+}
+
+// } Driver Code Ends
