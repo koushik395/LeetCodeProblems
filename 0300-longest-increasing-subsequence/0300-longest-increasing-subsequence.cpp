cefbe533b333fbf6a719969d1e44dff8f109f66e
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-07-26 23:10:36 +0530

    Time: 11 ms (87.41%), Space: 10.3 MB (96.96%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
+    // int n = nums.size();
+    //     vector<int> dp(n,1);
+    //     int maxi = -1;
+    //     for(int i = 0;i < n;i++)
+    //     {
+    //         int currMax = 1;
+    //         for(int j = 0;j < i;j++)
+    //         {
+    //             if(nums[j] < nums[i])
+    //             {
+    //                 currMax = max(currMax,dp[j]+dp[i]);
+    //             }
+    //         }
+    //         dp[i] = currMax;
+    //         maxi = max(maxi,dp[i]);
+    //     }
+    //     return maxi;
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n,1);
-        int maxi = -1;
-        for(int i = 0;i < n;i++)
-        {
-            int currMax = 1;
-            for(int j = 0;j < i;j++)
-            {
-                if(nums[j] < nums[i])
-                {
-                    currMax = max(currMax,dp[j]+dp[i]);
-                }
+        vector<int> sub;
+        for (int x : nums) {
+            if (sub.empty() || sub[sub.size() - 1] < x) {
+                sub.push_back(x);
+            } else {
+                auto it = lower_bound(sub.begin(), sub.end(), x); // Find the index of the first element >= x
+                *it = x; // Replace that number with x
             }
-            dp[i] = currMax;
-            maxi = max(maxi,dp[i]);
         }
-        return maxi;
+        return sub.size();
     }
 };
