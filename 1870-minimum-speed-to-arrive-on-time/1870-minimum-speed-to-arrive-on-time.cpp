36b5e232c64e5dbdab0837243f5768a7efa3e73b
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-07-26 22:30:50 +0530

    Time: 276 ms (92.52%), Space: 101.5 MB (26.33%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& dist,double& hour,int speed)
+    {
+        int n = dist.size();
+        double minSpeed = 0;
+        for(int i = 0;i < n-1;i++)
+        {
+            minSpeed += ceil((dist[i]*1.0)/speed);
+        }
+        minSpeed += ceil(dist[n-1]*1.0)/speed;
+        return minSpeed <= hour;
+    }
+    int minSpeedOnTime(vector<int>& dist, double hour) {
+        int low = 1,high = 1e7,ans = -1;
+        
+        while(low <= high)
+        {
+            int mid = (low + high)/2;
+            if(isPossible(dist,hour,mid))
+            {
+                ans = mid;
+                high = mid-1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return ans;
+            
+    }
+};
