ace791e796c239f9a551ac8cb941034c1bf8a7ef
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-07-27 15:23:09 +0530

    Time: 171 ms (85.97%), Space: 55.9 MB (36.33%) - LeetHub

---
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& batteries,long long time,long long comp)
+    {
+        long long totalTime = 0;
+        
+        for(auto& btime:batteries)
+        {
+            if(btime < time) totalTime += btime;
+            else totalTime += time;
+        }
+        
+        return (totalTime >= time * comp);
+    }
+    long long maxRunTime(int n, vector<int>& batteries) {
+        long long low = 1,high;
+        for(auto& btime: batteries)
+        {
+            high += btime;
+        }
+        
+        long long ans = 1;
+        while(low <= high)
+        {
+            long long mid = low + (high - low)/2;
+            if(isPossible(batteries,mid,n))
+            {
+                ans = mid;
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
