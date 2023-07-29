e12789336559211f421aa6ca86f14e056023df30
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-07-29 15:10:30 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -30,8 +30,9 @@ public:
         }
         for(int i = N-1;i >= 1;i--)
         {
-            for(int j = i+1;j < N;j++)
+            for(int j = 1;j < N;j++)
             {
+                if(i >= j) continue;
                 int mini = 1e9;
                 for(int k = i;k < j;k++)
                 {
