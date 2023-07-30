cf6dbda8e3e2cb7eb692e0e5fae29cbd500f3455
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-07-30 17:45:46 +0530

    Time: 33 ms (93.27%), Space: 6.4 MB (95.15%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int lcs(string text1, string text2,int m,int n)
     {
-        vector<int> prev(m+1,0),curr(n+1,0);
-        for(int i=0;i<=m;i++)
+        vector<int> prev(n+1,0),curr(n+1,0);
+        for(int i=1;i<=m;i++)
         {
-            for(int j=0;j<=n;j++)
+            for(int j=1;j<=n;j++)
             {
-                if(i==0 || j==0)
-                    curr[j] =0;
-                else if(text1[i-1] == text2[j-1])
+                // if(i==0 || j==0)
+                //     curr[j] =0;
+                if(text1[i-1] == text2[j-1])
                     curr[j] = 1+ prev[j-1];
                 else
                 {
