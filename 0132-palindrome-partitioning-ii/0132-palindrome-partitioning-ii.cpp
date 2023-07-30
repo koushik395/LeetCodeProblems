92ff6aca21a4edac7f0bacee7dd35030c1ab2818
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-07-30 19:52:59 +0530

    Time: 1274 ms (58.88%), Space: 6.4 MB (88.78%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -10,20 +10,20 @@ public:
         }
         return true;
     }
-    int f(int i,int n,string &s,vector<int> &dp)
-    {
-        if(i == n) return 0;
-        if(dp[i] != -1) return dp[i];
-        int mini = 1e9;
-        for(int j = i;j < n;j++)
-        {
-            if(isPalindrome(i,j,s))
-            {
-                mini = min(mini,1 + f(j+1,n,s,dp));
-            }
-        }
-        return dp[i] = mini;
-    }
+    // int f(int i,int n,string &s,vector<int> &dp)
+    // {
+    //     if(i == n) return 0;
+    //     if(dp[i] != -1) return dp[i];
+    //     int mini = 1e9;
+    //     for(int j = i;j < n;j++)
+    //     {
+    //         if(isPalindrome(i,j,s))
+    //         {
+    //             mini = min(mini,1 + f(j+1,n,s,dp));
+    //         }
+    //     }
+    //     return dp[i] = mini;
+    // }
     int minCut(string s) {
         int n = s.length();
         vector<int> dp(n+1,0);
