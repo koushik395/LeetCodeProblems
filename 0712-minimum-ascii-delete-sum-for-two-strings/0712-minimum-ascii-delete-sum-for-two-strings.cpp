36d29ef2db92e9319f1155af89ba4a8dde46ec3f
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-07-31 22:01:07 +0530

    Time: 18 ms (95.30%), Space: 6.7 MB (96.88%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,69 +1,68 @@
 class Solution {
 public:
-    int n, m;
-    int solve(string& s1, string& s2, int i, int j,vector<vector<int>> &dp)
-    {
-        if(i == 0 && j == 0)
-        {
-            return 0;
-        }
-        if(i == 0) // j is still there so delete them.
-        {
-            int cost = 0;
-            for(int k=1; k<=j; k++)
-            {
-                cost += (int)s2[k-1];
-            }
-            return cost;
-        }
-        if(j == 0) // i is still there so delete them.
-        {
-            int cost = 0;
-            for(int k=1; k<=i; k++)
-            {
-                cost += (int)s1[k-1];
-            }
-            return cost;
-        }
+    // int n, m;
+//     int solve(string& s1, string& s2, int i, int j,vector<vector<int>> &dp)
+//     {
+//         if(i == 0 && j == 0)
+//         {
+//             return 0;
+//         }
+//         if(i == 0) // j is still there so delete them.
+//         {
+//             int cost = 0;
+//             for(int k=1; k<=j; k++)
+//             {
+//                 cost += (int)s2[k-1];
+//             }
+//             return cost;
+//         }
+//         if(j == 0) // i is still there so delete them.
+//         {
+//             int cost = 0;
+//             for(int k=1; k<=i; k++)
+//             {
+//                 cost += (int)s1[k-1];
+//             }
+//             return cost;
+//         }
         
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s1[i-1] == s2[j-1])
-        {
-            return dp[i][j] = solve(s1, s2, i-1, j-1,dp);
-        }
-        int delstr1 = (int)s1[i-1] + solve(s1, s2, i-1, j,dp);
-        int delstr2 = (int)s2[j-1] + solve(s1, s2, i, j-1,dp);
-        return dp[i][j] = min(delstr1, delstr2);
-    }
+//         if(dp[i][j] != -1) return dp[i][j];
+//         if(s1[i-1] == s2[j-1])
+//         {
+//             return dp[i][j] = solve(s1, s2, i-1, j-1,dp);
+//         }
+//         int delstr1 = (int)s1[i-1] + solve(s1, s2, i-1, j,dp);
+//         int delstr2 = (int)s2[j-1] + solve(s1, s2, i, j-1,dp);
+//         return dp[i][j] = min(delstr1, delstr2);
+//     }
     int minimumDeleteSum(string s1, string s2) {
-        n = s1.length();
-        m = s2.length();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        int n = s1.length();
+        int m = s2.length();
+        vector<int> curr(m+1,0),prev(m+1,0);
         
         for(int j=1; j<=m; j++)
         {
-            dp[0][j] = dp[0][j-1] + (int)s2[j-1];
+            prev[j] = prev[j-1] + (int)s2[j-1];
         }
-        for(int i=1; i<=n; i++)
-        {
-            dp[i][0] = dp[i-1][0] + (int)s1[i-1];
-        }
+
         for(int i = 1;i <= n;i++)
         {
+            curr[0] = prev[0] + (int)s1[i-1];
             for(int j = 1;j <= m;j++)
             {
                 if(s1[i-1] == s2[j-1])
                 {
-                    dp[i][j] = dp[i-1][j-1];
+                    curr[j] = prev[j-1];
                 }
                 else
                 {
-                    int delstr1 = (int)s1[i-1] + dp[i-1][j];
-                    int delstr2 = (int)s2[j-1] + dp[i][j-1];
-                    dp[i][j] = min(delstr1, delstr2);   
+                    int delstr1 = (int)s1[i-1] + prev[j];
+                    int delstr2 = (int)s2[j-1] + curr[j-1];
+                    curr[j] = min(delstr1, delstr2);   
                 }
             }
+            prev = curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
