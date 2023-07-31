7c17ba9ae94e117dfbf371268e073a91dd3d809d
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-07-31 23:15:18 +0530

    Added solution - LeetHub

---
+++ Longest Palindromic Substring - GFG/longest-palindromic-substring.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    string longestPalindrome(string s){
+        int str_len = s.length();
+
+		if (str_len == 0 || str_len == 1)
+			return s;
+
+		string longest = "";
+		for (int i = 0; i < str_len - 1; i++)
+		{
+			int low_pt = i;
+			int high_pt = i + 1;
+
+			while (low_pt >= 0 && high_pt < str_len)
+			{
+				if (low_pt == i && s[low_pt] == s[high_pt])
+				{
+					high_pt++;
+					continue;
+				}
+
+				if (low_pt > 0 && s[low_pt - 1] == s[high_pt])
+				{
+					low_pt--;
+					high_pt++;
+				}
+
+				else
+					break;
+			}
+
+			string sub = s.substr(low_pt, high_pt - low_pt);
+
+			if (longest.length() < sub.length())
+				longest = sub;
+		}
+
+		return longest;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string S;
+        cin>>S;
+        Solution ob;
+        cout<<ob.longestPalindrome(S)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
