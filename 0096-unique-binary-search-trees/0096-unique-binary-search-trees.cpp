aadb2eff924abf0e23a0856d63e63d3525641aa8
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-08-05 14:58:31 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (35.20%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int numTrees(int n) {
-        if(n <= 1) return 1;
+        vector<int> dp(n+1,0);
+        dp[0] = dp[1] = 1;
         
-        int ans = 0;
-        for(int i = 1;i <= n;i++)
+        // for every i as root node
+        for(int i = 2;i <= n;i++)
         {
-            ans += numTrees(i-1) * numTrees(n-i);
+            for(int j = 1;j <= i;j++)
+            {
+                dp[i] += dp[j-1] * dp[i-j];
+            }   
         }
-        return ans;
+        return dp[n];
     }
 };
