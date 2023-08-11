8faa6d8dccfb902f3df68faac3c0fbaa7c43d3af
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-08-11 20:06:06 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -5,28 +5,31 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    long long solve(int ind,int tar,int coins[],vector<vector<long long>> &dp)
+    long long f(int coins[],int target, int i,vector<vector<long long>>& dp)
     {
-        if(ind == 0)
+        if(i == 0)
         {
-            return (tar%coins[ind] == 0);
+            return target%coins[0] == 0;
         }
         
-        if(dp[ind][tar] != -1) return dp[ind][tar];
         
-        long long nottake = solve(ind-1,tar,coins,dp);
+        if(dp[i][target]!=-1)
+           return dp[i][target];
         
-        long long take = 0;
-        if(coins[ind] <= tar)
-        {
-            take = solve(ind,tar-coins[ind],coins,dp);
-        }
+        long long nottake=f(coins,target,i-1,dp);
         
-        return dp[ind][tar] = take + nottake;
-    }
-    long long int count(int coins[], int N, int sum) {
-        vector<vector<long long>> dp(N,vector<long long>(sum+1,-1));
-        return solve(N-1,sum,coins,dp);
+        long long take=0;
+        if(coins[i]<=target)
+            take=f(coins,target-coins[i],i,dp);
+        
+        long long ans=take+nottake;
+        
+        return dp[i][target] = ans;
+   }
+    long long int count(int coins[], int N, int sum) 
+    {
+         vector<vector<long long>> dp(N,vector<long long>(sum+1,-1));
+        return f(coins,sum,N-1,dp);
     }
 };
 
