8a846ccad584aebe92fbf8ba1449e4179f9ac451
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-08-24 13:13:07 +0530

    Added solution - LeetHub

---
+++ Multiply two strings - GFG/multiply-two-strings.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+ 
+
+// } Driver Code Ends
+class Solution{
+  public:
+    /*You are required to complete below function */
+    string multiplyStrings(string num1, string num2) {
+        bool negative = false;
+        if (num1[0] == '-') {
+            negative = !negative;
+            num1 = num1.substr(1);
+        }
+        if (num2[0] == '-') {
+            negative = !negative;
+            num2 = num2.substr(1);
+        }
+        
+       if (num1 == "0" || num2 == "0") return "0";
+        
+        vector<int> res(num1.size()+num2.size(), 0);
+        
+        for (int i = num1.size()-1; i >= 0; i--) {
+            for (int j = num2.size()-1; j >= 0; j--) {
+                res[i + j + 1] += (num1[i]-'0') * (num2[j]-'0');
+                res[i + j] += res[i + j + 1] / 10;
+                res[i + j + 1] %= 10;
+            }
+        }
+        
+        int i = 0;
+        string ans = "";
+        while (res[i] == 0) i++;
+        while (i < res.size()) ans += to_string(res[i++]);
+        
+        if (negative && ans != "0") {
+            ans = "-" + ans;
+        }
+        
+        return ans;
+    }
+
+};
+
+//{ Driver Code Starts.
+ 
+int main() {
+     
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    	string a;
+    	string b;
+    	cin>>a>>b;
+    	Solution obj;
+    	cout<<obj.multiplyStrings(a,b)<<endl;
+    }
+     
+}
+// } Driver Code Ends
