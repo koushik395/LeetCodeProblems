fd5127100442ff3c6dac5178d08028103cb7ffe3
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-08-30 11:57:23 +0530

    Time: 46 ms (22.57%), Space: 18.8 MB (26.85%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     int bestClosingTime(string customers) {
-        int max_score = 0, score = 0, best_hour = -1;
-        for(int i = 0; i < customers.size(); ++i) {
-            score += (customers[i] == 'Y') ? 1 : -1;
-            if(score > max_score) {
-                max_score = score;
-                best_hour = i;
+        int n = customers.size();
+        vector<int> pre(n+1,0),suf(n+1,0);
+        for(int i = 0;i < n;i++)
+        {
+            if(customers[i] == 'N') pre[i+1] = pre[i] + 1;
+            else pre[i+1] = pre[i];
+            
+            if(customers[n-i-1] == 'Y') suf[n-i-1] = suf[n-i] + 1;
+            else suf[n-i-1] = suf[n-i];
+        }
+        int ans = 1e9,ind = 0;
+        for(int i = 0;i < n+1;i++)
+        {
+            if(pre[i] + suf[i] < ans)
+            {
+                ans = pre[i] + suf[i];
+                ind = i;
             }
         }
-        return best_hour + 1;
+        return ind;
     }
 };
