d688472171364c945fa21b8ed63741d8a4634477
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-08-30 12:37:00 +0530

    Time: 14 ms (99.35%), Space: 10.7 MB (86.38%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -2,24 +2,42 @@ class Solution {
 public:
     int bestClosingTime(string customers) {
         int n = customers.size();
-        vector<int> pre(n+1,0),suf(n+1,0);
+        int ind = -1,cnt = 0,maxi = 0;
         for(int i = 0;i < n;i++)
         {
-            if(customers[i] == 'N') pre[i+1] = pre[i] + 1;
-            else pre[i+1] = pre[i];
-            
-            if(customers[n-i-1] == 'Y') suf[n-i-1] = suf[n-i] + 1;
-            else suf[n-i-1] = suf[n-i];
-        }
-        int ans = 1e9,ind = 0;
-        for(int i = 0;i < n+1;i++)
-        {
-            if(pre[i] + suf[i] < ans)
+            cnt += (customers[i] == 'Y') ? 1 : -1;
+            if(cnt > maxi)
             {
-                ans = pre[i] + suf[i];
+                maxi = cnt;
                 ind = i;
             }
         }
-        return ind;
+        return ind+1;
     }
+//     int bestClosingTime(string customers) {
+//         int n = customers.size();
+//         vector<int> pre(n+1,0),suf(n+1,0);
+//         for(int i = 0;i < n;i++)
+//         {
+//             if(customers[i] == 'N') pre[i+1] = pre[i] + 1;
+//             else pre[i+1] = pre[i];
+            
+//             if(customers[n-i-1] == 'Y') suf[n-i-1] = suf[n-i] + 1;
+//             else suf[n-i-1] = suf[n-i];
+//         }
+//         int ans = 1e9,ind = 0;
+//         for(int i = 0;i < n+1;i++)
+//         {
+//             if(pre[i] + suf[i] < ans)
+//             {
+//                 ans = pre[i] + suf[i];
+//                 ind = i;
+//             }
+//         }
+//         for(int i = 0;i < n+1;i++)
+//         {
+//             cout << pre[i] << " "<<suf[i]<<endl;
+//         }
+//         return ind;
+//     }
 };
