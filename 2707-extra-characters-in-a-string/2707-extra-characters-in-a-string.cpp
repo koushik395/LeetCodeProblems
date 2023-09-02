8a21f92d992bf48a9c070a5cdef355479394ec4d
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-09-02 14:06:16 +0530

    Time: 161 ms (60.85%), Space: 82.6 MB (46.73%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -20,9 +20,23 @@ public:
     int minExtraChar(string s, vector<string>& dictionary) {
         n = s.size();
         unordered_map<string,int> mp;
-        vector<int> dp(n,-1);
+        vector<int> dp(n+1,0);
         for(auto& dic:dictionary) mp[dic]++;
         
-        return find(s,mp,dp,0);
+        for(int i = n-1;i >= 0;i--)
+        {
+            int mini = n;
+            for(int j = i;j < n;j++)
+            {
+                string word = s.substr(i,j - i + 1);
+                int extra = mp.count(word) ? 0: word.size();
+                int next = dp[j+1];
+
+                mini = min(mini,extra + next);
+            }
+            dp[i] = mini;
+        }
+        
+        return dp[0];
     }
 };
