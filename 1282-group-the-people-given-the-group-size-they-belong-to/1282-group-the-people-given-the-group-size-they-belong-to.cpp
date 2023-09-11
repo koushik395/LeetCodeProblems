4d8b0bfd2c4799475e609b2f08d5b24a8eeb380d
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-09-11 20:43:24 +0530

    Time: 17 ms (20.82%), Space: 13.8 MB (24.77%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -27,4 +27,14 @@ public:
         }
         return ans;
     }
+    
+      // vector<vector<int>> res, groups(gz.size() + 1);
+      // for (auto i = 0; i < gz.size(); ++i) {
+      //   groups[gz[i]].push_back(i);
+      //   if (groups[gz[i]].size() == gz[i]) {
+      //     res.push_back({});
+      //     swap(res.back(), groups[gz[i]]);
+      //   }
+      // }
+      // return res;
 };
