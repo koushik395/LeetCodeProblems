5cd10fdc91f3037c693d88fbe062d3d19a4d632b
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-10-02 18:18:29 +0530

    Added solution - LeetHub

---
+++ Number of distinct subsequences - GFG/number-of-distinct-subsequences.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:	
+	int distinctSubsequences(string s)
+	{
+	    long endsWith[26] = {}, mod = 1e9 + 7;
+        for (char c : s)
+            endsWith[c - 'a'] = accumulate(begin(endsWith), end(endsWith), 1L) % mod;
+        return accumulate(begin(endsWith), end(endsWith), 0L) % mod + 1;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+        Solution ob;
+   		cout << ob.distinctSubsequences(s) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends
