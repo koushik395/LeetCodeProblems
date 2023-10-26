d2c97298903a42bfe7475f747c1e62923b9f2800
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-10-26 18:11:09 +0530

    Added solution - LeetHub

---
+++ Minimum Operations - GFG/minimum-operations.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    int minOperation(int n)
+    {
+        if(n == 1) return 1;
+        int cnt = 0;
+        
+        while(n > 0){
+            if(n & 1) n -= 1;
+            else n /= 2;
+            cnt++;
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	    {
+	        int n;
+	        cin>>n;
+	        Solution ob;
+	        cout<<ob.minOperation(n)<<endl;
+	    }
+}
+// } Driver Code Ends
