86d949a865868794685f21408a8ede96d5ce8ca0
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-11-26 20:46:47 +0530

    Time: 108 ms (8.60%), Space: 88.1 MB (24.46%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -16,7 +16,11 @@ public:
         {
             int preVal = i == 0 ? 0 : pre[i-1];
             ans[i] = (nums[i] * i - preVal) + ((pre[n-1]-pre[i]) - nums[i]*(n-i-1));
+            
         }
         return ans;
     }
 };
+
+
+// For nums[i], the answer is (nums[i] - nums[0]) + (nums[i] - nums[1]) + ... + (nums[i] - nums[i-1]) + (nums[i+1] - nums[i]) + (nums[i+2] - nums[i]) + ... + (nums[n-1] - nums[i]).
