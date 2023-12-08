5de873c46b9238d4f46d8eaddf9f9b7fc9b5631e
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-12-08 11:35:18 +0530

    Time: 11 ms (82.27%), Space: 23.8 MB (89.04%) - LeetHub

--- 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.cpp
+++ 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.cpp
@@ -16,15 +16,15 @@ public:
         if (!root) return;   
         ans += to_string(root->val);
         if (root->left) { //left side check
-            ans += '(';
+            ans.push_back('(');
             preorder(root->left, ans);
-            ans += ')';
+            ans.push_back(')');
         }
         if (root->right) { //right side check
             if (!root->left) ans += "()"; //left side not present, but right side present
-            ans += '(';
+            ans.push_back('(');
             preorder(root->right, ans);
-            ans += ')';
+            ans.push_back(')');
         }
     }
     string tree2str(TreeNode* root) {
