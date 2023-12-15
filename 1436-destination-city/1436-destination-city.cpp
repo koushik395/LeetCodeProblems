ab8875d7bd9f9470d30c9d67fea3b3eac29f3571
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2023-12-15 12:51:02 +0530

    Time: 8 ms (75.96%), Space: 10.9 MB (71.53%) - LeetHub

--- 1436-destination-city/1436-destination-city.cpp
+++ 1436-destination-city/1436-destination-city.cpp
@@ -9,7 +9,8 @@ public:
         
         for(auto& it:paths)
         {
-            if(mp.find(it[1]) == mp.end()) return it[1];
+            if(mp.find(it[1]) == mp.end()) 
+                return it[1];
         }
         return "";
     }
