178e57750881e9e916c9ca9cb9e5fac84a8ab6c2
Koushik Boddapalli <81212573+koushik395@users.noreply.github.com>
2024-01-01 19:40:13 +0530

    Time: 39 ms (9.60%), Space: 17.7 MB (75.56%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(begin(g),end(g));
+        sort(begin(s),end(s));
+        int i = 0;
+        for(int j=0;i<g.size() && j<s.size();j++) {
+            if(g[i]<=s[j]) i++;
+        }
+        return i;
+    }
+};
